cb5364394afccd74466313d06145d03b902dea90
leeyaonan <leeyaonan@outlook.com>
2020-04-26 14:41:34 +0800

    搭建springboot项目

---
+++ src/test/java/com/leeyaonan/idempotent/IdempotentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeyaonan.idempotent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
